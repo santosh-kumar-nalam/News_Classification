f4336b903daf8f1b1f4bb0075421c73bfa19ed24
Nalam Santosh Kumar <167790146+santosh-kumar-nalam@users.noreply.github.com>
2024-06-25 22:35:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,56 @@
-import streamlit as st  
+import streamlit as st
 import pickle
-loaded_model = pickle.load(open('News_classification.sav','rb')) 
-from sklearn.ensemble import RandomForestClassifier 
-import re 
+import re
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
-from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
-nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-nltk.download('punkt') 
 
-vectorize = pickle.load(open('CountVectorize.sav','rb'))
-model = pickle.load(open('News_classification.sav','rb'))
- 
+# Ensure NLTK data is downloaded
+nltk.data.path.append('./nltk_data')
+nltk.download('stopwords', download_dir='./nltk_data')
+nltk.download('wordnet', download_dir='./nltk_data')
+nltk.download('punkt', download_dir='./nltk_data')
 
+# Load the pre-trained model and vectorizer
+loaded_model = pickle.load(open('News_classification.sav', 'rb'))
+vectorize = pickle.load(open('CountVectorize.sav', 'rb'))
 
+# Preprocess the input text
 def preprocess(text):
-    remove = re.compile(r'')
-    after_removed_tags= re.sub(remove, '', text) 
-    removed_special_char = ''
-    for x in after_removed_tags:
-        if x.isalnum():
-            removed_special_char = removed_special_char + x
-        else:
-            removed_special_char = removed_special_char + ' '
-    lowered_text = removed_special_char.lower()  
-    
-    stop_words = set(stopwords.words('english')) 
-    
-    words = word_tokenize(lowered_text)
-    after_stopwords = [x for x in words if x not in stop_words]  
-    
-    wordnet = WordNetLemmatizer()
-    return " ".join([wordnet.lemmatize(word) for word in after_stopwords]) 
+    # Remove special characters and tags
+    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = text.lower().strip()
 
+    # Tokenize and remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
 
-st.title("News Classification") 
-input_text = st.text_area('Enter your News article text') 
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+
+    return ' '.join(words)
+
+# Streamlit UI
+st.title("News Classification")
+input_text = st.text_area('Enter your News article text')
+
 if st.button('Predict'):
+    # 1. Preprocess the input text
+    preprocessed_text = preprocess(input_text)
 
-    # 1. preprocess
-    prepcoess_text = preprocess(input_text) 
-    # 2. vectorize
-    vector_input_text = vectorize.transform([prepcoess_text])
-    # 3. predict
-    output = model.predict(vector_input_text) 
-    # 4. Display
-    if output == [0]:
-        result = "Business News"
-    elif output == [1]:
-        result = "Tech News"
-    elif output == [2]:
-        result = "Politics News"
-    elif output == [3]:
-        result = "Sports News"
-    elif output == [1]:
-        result = "Entertainment News"
+    # 2. Vectorize the preprocessed text
+    vectorized_text = vectorize.transform([preprocessed_text])
+
+    # 3. Predict the category
+    output = loaded_model.predict(vectorized_text)
+
+    # 4. Display the result
+    
+    categories = {0: "Business News", 1: "Tech News", 2: "Politics News", 3: "Sports News", 4: "Entertainment News"}
+    result = categories.get(output[0], "Unknown Category")
     st.header(result)
-
-
